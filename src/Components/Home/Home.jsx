556c05eead265dbab4b578939deb1fa9ad617b73
Roman <sheikhroman042@gmail.com>
2023-01-08 17:26:22 +0600

    loding

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import './home.css'
 import video from '../../Assets/banner.mp4'
 import {GrLocation} from 'react-icons/gr'
+import { FaSearchLocation,FaTripadvisor } from 'react-icons/fa'
+import {FiFacebook} from 'react-icons/fi'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { BsListTask } from 'react-icons/bs'
+import {TbApps} from 'react-icons/tb'
 
 const Home = () => {
   return (
@@ -24,6 +29,36 @@ const Home = () => {
               <input type="text" placeholder='Enter name here....' />
               <GrLocation className="icon" />
             </div>
+          </div>
+          <div className="dateInput">
+            <label htmlFor="date">Select your date: </label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+          <div className="priceInput">
+            <div className="lable_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className="total">$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000" />
+            </div>
+          </div>
+          <div className="searchOption flex">
+            <FaSearchLocation className="icon" />
+            <span>MORE FILTERS</span>
+          </div>
+        </div>
+        <div className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <FiFacebook  className="icon"/>
+            <AiOutlineInstagram  className="icon"/>
+            <FaTripadvisor  className="icon"/>
+          </div>
+          <div className="leftIcons">
+            <BsListTask className="icon"/>
+            <TbApps className="icon"/>
           </div>
         </div>
       </div>

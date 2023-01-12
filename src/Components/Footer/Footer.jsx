330c6d83976a5216332f900cdf3ecd5e2be48f4d
Roman <sheikhroman042@gmail.com>
2023-01-12 13:00:14 +0600

    add some css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './footer.css'
 import video from '../../Assets/video.mp4'
-import {FiSend} from 'react-icons/fi'
+import {FiSend, FiChevronRight} from 'react-icons/fi'
 import {MdOutlineTravelExplore} from 'react-icons/md'
+import { AiOutlineTwitter, AiFillYoutube, AiFillInstagram } from 'react-icons/ai'
+import {FaTripadvisor} from 'react-icons/fa'
 const Footer = () => {
   return (
     <section className="footer">
@@ -29,6 +31,98 @@ const Footer = () => {
                 <MdOutlineTravelExplore  className="icon"/>  Travel.
               </a>
             </div>
+            <div className="footerParagraph">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod architecto facere ab esse debitis, quidem nostrum hic praesentium neque ratione.
+            </div>
+            <div className="footerSocail flex">
+            <AiOutlineTwitter className="icon"/>
+            <AiFillYoutube className="icon"/>
+            <AiFillInstagram className="icon"/>
+            <FaTripadvisor className="icon"/>
+            </div>
+          </div>
+          <div className="footerLinks grid">
+            {/* Group ONE */}
+            <div className="linkGroup">
+              <span className="groupTitle">
+                OUR AGENCY
+              </span>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Toursmn
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+            {/* Group THREE */}
+            <div className="linkGroup">
+              <span className="groupTitle">
+              PARTNRS
+              </span>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Bookings
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Rentcars
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                HostelWorld
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Trivago
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                TripSdvisor
+              </li>
+            </div>
+            {/* Group TWO */}
+            <div className="linkGroup">
+              <span className="groupTitle">
+                LAST MINUTE
+              </span>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                London
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                California
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Indonesia
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Europe
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Oceania
+              </li>
+            </div>
+          </div>
+          <div className="footerDiv flex">
+
           </div>
         </div>
       </div>
